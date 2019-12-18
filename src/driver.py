19e8ce8cc60a6a4d3e13253b8761c3924fb99110
Zach <romerzs14@gmail.com>
2019-12-17 23:20:23 -0800

    Moved exception clause to encapsulate entire code and quit driver properly if exception is encountered

--- src/driver.py
+++ src/driver.py
@@ -9,37 +9,44 @@ from time import sleep
 
 
 def sneak_into_aws():
-
-    driver = webdriver.Firefox()
-
-    driver.get('http://console.aws.amazon.com')
-
-    account_input_box = driver.find_element_by_id('resolving_input')
-    account_input_box.send_keys(os.getenv('AWS_ADMIN'))
-    account_input_box.send_keys(Keys.RETURN)
-
+    
     try:
-        user_input_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'username')))
-    except:
-        driver.quit()
-    user_input_box.send_keys(os.getenv('AWS_USER'))
+        driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+        driver.get('http://console.aws.amazon.com')
+    
+        account_input_box = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'resolving_input')))
+        account_input_box.send_keys(os.getenv('AWS_ADMIN'))
+        account_input_box.send_keys(Keys.RETURN)
 
-    sleep(5)
+        user_input_box = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'username')))
+        user_input_box.send_keys(os.getenv('AWS_USER'))
 
-    password_input_box = driver.find_element_by_id('password')
-    password_input_box.send_keys(os.getenv('AWS_PASSWORD'))
-    password_input_box.send_keys(Keys.RETURN)
-    try:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'awsc-mezz-data')))
-    except:
-        driver.quit()
+        password_input_box = driver.find_element_by_id('password')
+        password_input_box.send_keys(os.getenv('AWS_PASSWORD'))
+        password_input_box.send_keys(Keys.RETURN)
+    
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.NAME, 'awsc-mezz-data')))
+    
+        driver.get('https://us-west-2.console.aws.amazon.com/xray/home?region=us-west-2#/service-map?timeRange=PT1H')
+        driver.maximize_window()
+    
+        while True:
+            services = ['fastq-registrar-staging']
+            zoom_on_service(
 
-    driver.get('https://us-west-2.console.aws.amazon.com/xray/home?region=us-west-2#/service-map?timeRange=PT1H')
-    driver.maximize_window()
-    while True:
-        sleep(15)
-        refresh_button = driver.find_elements_by_css_selector(".awsui-button-group-content > span:nth-child(1) > span:nth-child(1) > awsui-button:nth-child(1) > button:nth-child(1)")
-        refresh_button[0].send_keys(Keys.RETURN)
+            sleep(300)
+            refresh_button = driver.find_elements_by_css_selector(".awsui-button-group-content > span:nth-child(1) > span:nth-child(1) > awsui-button:nth-child(1) > button:nth-child(1)")
+            refresh_button[0].send_keys(Keys.RETURN)
+    except Exception as exc:
+        print(f'Error occured while monitoring X-Ray: {exc}')
+
+    driver.quit()
+
+def zoom_on_service(service_name: str):
+   service_search_box = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'node-graph-filter-input')))
+    service_search_box.send_keys(service_name)
+    service_search_box.send_keys(Keys.RETURN)
+    
 
 
 def pull_up_dashboard():
